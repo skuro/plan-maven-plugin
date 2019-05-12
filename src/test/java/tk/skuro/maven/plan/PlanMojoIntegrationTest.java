7f9109a81c097e37c928dd9256c4563e7191d809
Carlo Sciolla <carlo.sciolla@gmail.com>
2019-05-12 22:57:09 +0200

    Maybe it works, maybe it doesn't

--- src/test/java/tk/skuro/maven/plan/PlanMojoIntegrationTest.java
+++ src/test/java/tk/skuro/maven/plan/PlanMojoIntegrationTest.java
@@ -7,10 +7,25 @@ import org.apache.maven.it.util.ResourceExtractor;
 import java.io.File;
 
 public class PlanMojoIntegrationTest extends TestCase {
+
+    private final static String defaultMavenHome = "/home/linuxbrew/.linuxbrew";
+
+    public void setUp(){
+        if(System.getProperty("maven.home") == null) {
+            String m2Home = System.getenv("M2_HOME");
+            if(m2Home != null) {
+                System.setProperty("maven.home", m2Home);
+            } else {
+                System.setProperty("maven.home", defaultMavenHome);
+            }
+        }
+    }
+
     public void testMyPlugin()
             throws Exception {
         File testDir = ResourceExtractor.simpleExtractResources(getClass(), "/test-project");
 
+        System.getenv("M2_HOME")
         System.setProperty("maven.home", "/home/linuxbrew/.linuxbrew");
         Verifier verifier = new Verifier(testDir.getAbsolutePath());
 
